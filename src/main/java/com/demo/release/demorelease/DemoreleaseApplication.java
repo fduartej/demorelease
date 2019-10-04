44c06eeae5092fe7699eedd851b627bb41e1ef38
Fred Duarte <gestion_atlassian@inteligogroup.com>
2019-10-04 15:02:47 -0500

    first commit

---
+++ src/main/java/com/demo/release/demorelease/DemoreleaseApplication.java
@@ -0,0 +1,13 @@
+package com.demo.release.demorelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoreleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoreleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/release/demorelease/DemoreleaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.release.demorelease;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoreleaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
